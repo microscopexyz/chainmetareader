93504ac41ac59f1578ebd2778583a1e416d8c522
Zhanwu Xiong <zhanwu.xiong@coinbase.com>
2023-03-07 21:00:06 -0800

    Lint

--- chainmeta_reader/metadata.py
+++ chainmeta_reader/metadata.py
@@ -1,44 +1,60 @@
-from typing import List
-from datetime import datetime
-from dataclasses import dataclass
-from enum import Enum
+# Copyright 2023 The chainmetareader Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import List
+
 
 class Namespace(Enum):
     ChainTool = 1
     Coinbase = 2
     GoPlus = 3
-    
+
+
 @dataclass
 class Network:
-    """Network represents a uniquely identified blockchain network, 
+    """Network represents a uniquely identified blockchain network,
     e.g. ethereum mainnet, or ethereum goerli
     """
 
     # The string representation of the network
     name: str
 
+
 @dataclass
 class Tag:
-    """Tag is a generic container for any information like name, 
+    """Tag is a generic container for any information like name,
     entity or category etc.
     """
-    
+
     # Namespace this tag belongs to
     namespace: Namespace
 
     # Type of the tag, e.g. entity, category or attribute
-    type: str               
-    
+    type: str
+
     # The string representation of the tag
     name: str
 
+
 @dataclass
 class MetadataItem:
-    """Intermediate representation of the metadata.
-    """
+    """Intermediate representation of the metadata."""
 
-    # Wallet or contract address 
+    # Wallet or contract address
     address: str
 
     # Network identification
@@ -57,14 +73,12 @@ class MetadataItem:
 class ITranslator(ABC):
     @abstractmethod
     def to_intermediate(self, raw_metadata) -> List[MetadataItem]:
-        """Translate to intermediate representation
-        """
+        """Translate to intermediate representation"""
         pass
 
     @abstractmethod
     def from_intermediate(self, intermediate_metadata: List[MetadataItem]) -> object:
-        """Translate from intermediate representation
-        """
+        """Translate from intermediate representation"""
         pass
 
 
