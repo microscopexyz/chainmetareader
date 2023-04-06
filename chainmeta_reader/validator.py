b9373a0bfec5c79625cc1269dfc890464ac5eeb5
Zhanwu Xiong <zhanwu.xiong@coinbase.com>
2023-04-05 21:18:34 -0700

    Consolidate validator

--- chainmeta_reader/validator.py
+++ chainmeta_reader/validator.py
@@ -12,34 +12,38 @@
 # limitations under the License.
 import json
 import pathlib
-from abc import ABC, abstractmethod
 
-from jsonschema import validate as js_validate
+from jsonschema import Draft7Validator, validators
 
-from chainmeta_reader.constants import ValidatorType
+from chainmeta_reader.config import Config, default_config
+from chainmeta_reader.constants import Field
 
 
-class IValidator(ABC):
-    @abstractmethod
-    def validate(self, metadata: dict):
-        pass
-
-    def __init__(self, *, validator_type: ValidatorType):
-        self.validator_type = validator_type
-
-    def to_string(self) -> str:
-        prefix = "chainmeta"
-        validator = "validator"
-        return f"{prefix}.{validator}.{self.validator_type}"
+def value_checker(valid_values):
+    return lambda checker, instance: (
+        checker.is_type(instance, "string") and instance in valid_values
+    )
 
 
 class JsonValidator(object):
-    def __init__(self, *, schema: dict):
+    def __init__(self, *, config: Config = default_config, schema: dict):
+        type_checker = Draft7Validator.TYPE_CHECKER.redefine_many(
+            {
+                Field.CATEGORY.value: value_checker(config.Categories),
+                Field.ENTITY.value: value_checker(config.Entities),
+                Field.SOURCE.value: value_checker(config.Sources),
+                Field.CHAIN.value: value_checker(config.Chains),
+            }
+        )
+
         with open(schema) as sf:
-            self.schema = json.load(sf)
+            custom_validator = validators.extend(
+                Draft7Validator, type_checker=type_checker
+            )
+            self.validator = custom_validator(schema=json.load(sf))
 
     def validate(self, metadata: dict):
-        js_validate(instance=metadata, schema=self.schema)
+        self.validator.validate(metadata)
 
 
 class ValidatorError(ValueError):
@@ -48,40 +52,15 @@ class ValidatorError(ValueError):
         self.msg = msg
 
 
-class Validator(IValidator):
+class Validator:
     def __init__(self):
-        self.validator_type = ValidatorType.Global
         schema_file = (
             pathlib.Path(__file__)
             .parent.resolve()
             .joinpath("schemas", "meta_schema.json")
         )
-        self._validators = [JsonValidator(schema=schema_file)]
+        self._validators = [JsonValidator(config=default_config, schema=schema_file)]
 
     def validate(self, metadata: dict):
         for v in self._validators:
             v.validate(metadata)
-
-
-class ChaintoolValidator(IValidator):
-    def __init__(self, config_rules):
-        super().__init__(validator_type=ValidatorType.ChainTool)
-        self.rules = config_rules
-
-    def validate(self, input_address):
-        # check the global format
-        super().validate(input_address)
-
-        # TODO add the chaintool related format checking logic here
-
-
-class GoPlusValidator(IValidator):
-    def __init__(self, config_rules):
-        super().__init__(validator_type=ValidatorType.GoPlus)
-        self.rules = config_rules
-
-    def validate(self, input_address):
-        # check the global format
-        super().validate(input_address)
-
-        # TODO add GoPlus related format checking logic
