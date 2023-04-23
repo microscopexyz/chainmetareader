acd656395a1e9a1395c28bfa34f283e5f4ca5644
YiDong <541879862@qq.com>
2023-04-24 01:04:16 +0800

    update readme.md and chaintool usage (#67)
    * 1. fix readme.md
    2. update chaintool usage
    
    * add blank line

--- usage.py
+++ usage.py
@@ -46,9 +46,9 @@ with open("./examples/coinbase_sample.json") as f:
         logging.info(item)
 
 # Load metadata from file with Chaintool schema and translate between schemas
-logging.info("5. Translate between schemas")
 with open("./examples/chaintool_sample.json") as f:
     # Load Chaintool artifact
+    logging.info("1. Chaintool: Load raw metadata")
     metadata = load(f, artifact_base_path="./examples")
 
     # Raw metadata
@@ -57,6 +57,7 @@ with open("./examples/chaintool_sample.json") as f:
         logging.info(item)
 
     # Common schema metadata
+    logging.info("2. Chaintool: Translate to common metadata")
     common_metadata = metadata["chainmetadata"]["artifact"]
     for item in common_metadata:
         logging.info(item)
@@ -64,8 +65,33 @@ with open("./examples/chaintool_sample.json") as f:
     # Translate back to Chaintool schema
     from chainmeta_reader.contrib.chaintool import ChaintoolTranslator
 
+    logging.info("3. Chaintool: Translate back to Chaintool schema")
     raw_metadata = [
         ChaintoolTranslator().from_common_schema(i) for i in common_metadata
     ]
     for item in raw_metadata:
+        logging.info(item)
+
+    # # Persist metadata to database
+    logging.info("4. Chaintool: Upload to database")
+    n = upload_chainmeta(common_metadata)
+    logging.info(f"Added {n} items to database")
+
+    # Retrieve metadata from database
+    logging.info("5. Chaintool: Search without filter")
+    result = search_chainmeta()
+    for i, item in enumerate(result):
+        if i >= 10:
+            break
+        logging.info(item)
+
+    # Retrieve metadata from database with filter
+    logging.info("6. Search with filter")
+    result = search_chainmeta(
+        filter={
+            "address": "0xd73330336d084FaDF67cF8DE0EF4fB138cD642C9",
+            "chain": "ethereum_mainnet",
+        }
+    )
+    for item in result:
         logging.info(item)
