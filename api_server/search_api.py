55a8ac762424bf14923f0077d18ebe3c9fe9be04
dongyi1991 <541879862@qq.com>
2023-06-01 12:43:36 +0800

    update api

--- api_server/search_api.py
+++ api_server/search_api.py
@@ -11,22 +11,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from chainmeta_reader import search_chainmeta
 
 router = APIRouter()
 
 
-@router.get("/search_chainmeta/{chain}/{address}")
-async def search(chain: str, address: str):
-    if address is None or chain is None:
-        return "missing parameter address or chain"
-    results = search_chainmeta(
-        filter={
-            "address": address,
-            "chain": chain,
-        }
-    )
-
+@router.get("/search_chainmeta")
+async def search(chain: str = Query(None), address: str = Query(None)):
+    if address is None:
+        return "missing parameter address"
+    if chain is None:
+        results = search_chainmeta(filter={"address": address})
+    else:
+        results = search_chainmeta(
+            filter={
+                "address": address,
+                "chain": chain,
+            }
+        )
     return results
