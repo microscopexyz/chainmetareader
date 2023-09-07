202d0e5bb19f54b20438e76af49133cf5126bedb
ryjiangx <ryjiangx@163.com>
2023-09-07 11:29:54 +0800

    fix pre-commit

--- scripts/diversity/ocm_diversity_metrics.py
+++ scripts/diversity/ocm_diversity_metrics.py
@@ -1,3 +1,7 @@
+"""
+ocm_diversity_metrics.py
+This module provides functions for calculating diversity metrics in the OCM dataset.
+"""
 #!/usr/bin/env python3
 
 # Copyright 2023 The chainmetareader Authors. All rights reserved.
@@ -13,59 +17,79 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 
-def entropy_calc(x):
-    """Takes in probability and calculates the entropy value"""
-    return -x * np.log(x)
+def entropy_calc(x_dist):
+    """
+    Takes in probability and calculates the entropy value
+    Args:
+        x_dist: numpy array which is a float vlaue array 
+    Returns: entropy for distribution x 
+    """
+    return -x_dist * np.log(x_dist)
 
 
-def get_diversity_metircs(ocm_count_by_entity_csv_file):
-    
-    df = pd.read_csv(ocm_count_by_entity_csv_file)
-    top_3_df = df.sort_values(by=["percentage_of_total"], ascending=False).head(3)
+def calc_diversity_metircs(ocm_count_by_entity_csv_file):
+    """
+    Takes in probability and calculates the entropy value
+    Args:
+        x_dist: numpy array which is a float vlaue array 
+    Returns: 
+        top_3: Categories Concentratio of OCM entities who contribute the labeled data 
+        gini_coefficient: Gini coefficient: of OCM entities who contribute the labeled data 
+        shannon_entropy: Shannon entropy of OCM entities who contribute the labeled data
+    """
+    data_frame = pd.read_csv(ocm_count_by_entity_csv_file)
+    top_3_df = data_frame.sort_values(by=["percentage_of_total"], ascending=False).head(3)
     top_3 = top_3_df["percentage_of_total"].sum()
-    
+
     # Sort the DataFrame by percentage_of_total in descending order
-    df = df.sort_values(by="percentage_of_total", ascending=False)
+    data_frame = data_frame.sort_values(by="percentage_of_total", ascending=False)
 
     # Calculate the cumulative sum of the percentage_of_total column
-    df["cumulative_percentage"] = df["percentage_of_total"].cumsum()
+    data_frame["cumulative_percentage"] = data_frame["percentage_of_total"].cumsum()
 
     # Calculate the proportion of total and cumulative proportion of tags
-    df["proportion_of_total"] = df["percentage_of_total"] / df["percentage_of_total"].sum()
-    df["cumulative_proportion"] = df["cumulative_percentage"] / df["percentage_of_total"].sum()
+    data_frame["proportion_of_total"] = (
+        data_frame["percentage_of_total"] / data_frame["percentage_of_total"].sum()
+    )
+    data_frame["cumulative_proportion"] = (
+        data_frame["cumulative_percentage"] / data_frame["percentage_of_total"].sum()
+    )
 
-    # Calculate the Lorenz curve; represents the cumulative proportion of the total on the y-axis 
+    # Calculate the Lorenz curve; represents the cumulative proportion of the total on the y-axis
     # and the cumulative proportion of tags on the x-axis
-    df["lorenz_curve"] = df["cumulative_proportion"]
+    data_frame["lorenz_curve"] = data_frame["cumulative_proportion"]
 
     # Calculate the area under the Lorenz curve
-    area_lorenz = df["lorenz_curve"].sum() / len(df)
+    area_lorenz = data_frame["lorenz_curve"].sum() / len(data_frame)
 
     # Calculate the area of the perfect equality line
     area_perfect_equality = 0.5
 
     # Calculate the Gini coefficient
-    gini_coefficient = abs((area_perfect_equality - area_lorenz) / area_perfect_equality)
+    gini_coefficient = abs(
+        (area_perfect_equality - area_lorenz) / area_perfect_equality
+    )
 
-    shannon_df = df.copy()
+    shannon_df = data_frame.copy()
 
-    # create entropy column by calling the entropy_calc() function on the proportion_of_total column 
+    # create entropy column by calling the entropy_calc() function on the proportion_of_total column
     # which represents 'probability'
-    shannon_df['entropy'] = shannon_df['proportion_of_total'].apply(entropy_calc)
+    shannon_df["entropy"] = shannon_df["proportion_of_total"].apply(entropy_calc)
 
     # sum the entropy column to get the Shannon entropy value
-    shannon_entropy = shannon_df['entropy'].sum()
-    
+    shannon_entropy = shannon_df["entropy"].sum()
+
     return top_3, gini_coefficient, shannon_entropy
 
 
-count_by_entity_csv = "ocm_count_by_entity.csv"
-top_3, gini_coefficient, shannon_entropy = get_diversity_metircs(count_by_entity_csv)
+COUNT_BY_ENTITY_CSV = "ocm_count_by_entity.csv"
+metric_top_3, metric_gini_coefficient, metric_shannon_entropy \
+    = calc_diversity_metircs(COUNT_BY_ENTITY_CSV)
 
-print("Top 3 Categories Concentration: ", top_3)
-print("Gini coefficient:", gini_coefficient)
-print("Shannon entropy:", shannon_entropy)
+print("Top 3 Categories Concentration: ", metric_top_3)
+print("Gini coefficient:", metric_gini_coefficient)
+print("Shannon entropy:", metric_shannon_entropy)
