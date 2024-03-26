04869671cf07e2f7e30e792166e318de9cabfc1d
Zhanwu Xiong <488359+zhanwu@users.noreply.github.com>
2024-03-25 18:35:29 -0700

    lint

--- dbmigration/versions/e7429574614a_add_gmt_create_and_gmt_modified.py
+++ dbmigration/versions/e7429574614a_add_gmt_create_and_gmt_modified.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The chainmetareader Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """add gmt_create and gmt_modified
 
 Revision ID: e7429574614a
@@ -6,9 +19,8 @@ Create Date: 2024-03-25 16:49:23.941699
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "e7429574614a"
--- dbmigration/versions/f1e13c4fa803_add_from_to_and_additional_info.py
+++ dbmigration/versions/f1e13c4fa803_add_from_to_and_additional_info.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The chainmetareader Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """add from to and additional info
 
 Revision ID: f1e13c4fa803
@@ -6,9 +19,8 @@ Create Date: 2024-03-25 17:08:08.491118
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "f1e13c4fa803"
